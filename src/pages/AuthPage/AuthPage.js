b15cb2e3eb855ec239dbf1bc09e35d4c7a0bd1f2
Rania Elnaggar <yousef_amir1@hotmail.com>
2022-12-20 22:41:35 -0500

    added function to auth pg

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,38 +1,46 @@
-import SignUpForm from "../../components/SignUpForm/SignUpForm"
-import LoginForm from "../../components/LoginForm/LoginForm"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './AuthPage.module.css'
-import Logo from '../../components/images/logo.jpeg'
-;
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { useState } from "react";
+import "./AuthPage.module.css";
+import Logo from "../../components/images/logo.jpeg";
+import { Button } from "bootstrap";
+
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+  const signUpBotton = () => {
+    setShowLogin(!showLogin);
+  };
+  return (
+    <>
+      <img src={Logo} style={{ width: "150pt" }} />
+      <div className="App">
+        <div className="container-fluid ps-md-0">
+          <div className="row g-0">
+            <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+            <div className="col-md-8 col-lg-6">
+              <div className="login d-flex align-items-center py-5">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-9 col-lg-8 mx-auto">
 
 
-export default function AuthPage({setUser}){
-    return(
-      <>
-        <img src={Logo} style={{ width: "150pt" }} />
-        <div className="App">
-          <div className="container-fluid ps-md-0">
-              <div className="row g-0">
-                <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-                <div className="col-md-8 col-lg-6">
-                  <div className="login d-flex align-items-center py-5">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-md-9 col-lg-8 mx-auto">
-        <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-        <SignUpForm setUser={setUser}/>
-        <LoginForm setUser={setUser} />
-        
-        </div>
-                      </div>
+                      {showLogin ? (
+                        <LoginForm setUser={setUser} />
+                      ) : (
+                        <SignUpForm setUser={setUser} />
+                      )}
+                           <button onClick={signUpBotton}>{showLogin ? 'SIGN UP' : 'LOG IN'}</button>
                     </div>
+                    
                   </div>
                 </div>
               </div>
             </div>
-            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
-
---
+++ src/pages/UserProfile/UserProfile.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function UserProfile() {
+    const [UserProfile , setProfile] = useState(null)
+  return (
+    <div>UserProfile</div>
+  )
+}
+
+export default UserProfile
